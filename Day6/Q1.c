1dc61ceeaa04224ac3bdb5b395bc080715c42214
ShivamSinghVats <sherkot.shivam@gmail.com>
2023-06-22 10:36:07 +0530

    Q1

--- Day6/Q1.c
+++ Day6/Q1.c
@@ -1,36 +1,58 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include<string.h>
 
-struct word {
-  char *word;
-  int length;
+struct Student {
+  int rollno;
+  char name[20];
+  float marks;
 };
 
-int parse_string(char *string, struct word *words, int max_words) {
-  int i = 0;
+void parse_string(char *str, struct Student *students, int n) {
   char *token;
+  int i;
 
-  token = strtok(string, " ");
-  while (token != NULL && i < max_words) {
-    words[i].word = token;
-    words[i].length = strlen(token);
-    i++;
+  for (i = 0; i < n; i++) {
+    token = strtok(str, " ");
+    students[i].rollno = atoi(token);
     token = strtok(NULL, " ");
+    strcpy(students[i].name, token);
+    token = strtok(NULL, " ");
+    students[i].marks = atof(token);
   }
+}
 
-  return i;
+void initialize_students(struct Student *students, int n) {
+  int i;
+
+  for (i = 0; i < n; i++) {
+    students[i].rollno = 0;
+    strcpy(students[i].name, "");
+    students[i].marks = 0.0;
+  }
 }
 
 int main() {
-  char *string = "This is a test string.";
-  struct word words[10];
-  int number_of_words;
+  int n;
+  struct Student *students;
+  char str[100];
 
-  number_of_words = parse_string(string, words, 10);
+  printf("Enter the number of students: ");
+  scanf("%d", &n);
 
-  for (int i = 0; i < number_of_words; i++) {
-    printf("Word: %s, Length: %d\n", words[i].word, words[i].length);
+  students = malloc(n * sizeof(struct Student));
+  initialize_students(students, n);
+
+  printf("Enter the student details (rollno name marks): ");
+  scanf("%s", &str);
+
+  parse_string(str, students, n);
+
+  for (int i = 0; i < n; i++) {
+    printf("Student %d: rollno=%d, name=%s, marks=%.2f\n", i + 1, students[i].rollno, students[i].name, students[i].marks);
   }
+
+  free(students);
 
   return 0;
 }
